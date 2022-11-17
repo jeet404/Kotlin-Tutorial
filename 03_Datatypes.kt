4af4f51a02d9698e8ac9ae8d9e17397c93e7f2a2
jeet404 <jeet63240@gmail.com>
2022-11-17 19:51:32 +0530

    Add Datatypes Code file.

---
+++ 03_Datatypes.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var num: Int = 10
+    var name: String = "Kt Lang"
+    var grade: Char = 'A'
+    var temp: Double = 10.3
+    var isPass: Boolean = true
+
+    println(num)
+    println(name)
+    println(grade)
+    println(temp)
+    println(isPass)
+}
