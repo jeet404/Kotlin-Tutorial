c07469565e291bed2d28a8a53b5b7f6b96a76c41
jeet404 <jeet63240@gmail.com>
2022-11-18 19:20:33 +0530

    Add Code for Logical Operators.

---
+++ 07_LogicalOperator.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var isCold = true
+    var isDry = false
+
+    // && (AND Operator)
+    var result = isCold && isDry
+    println(result)
+
+    // || (OR Operator)
+    result = isCold || isDry
+    println(result)
+}
