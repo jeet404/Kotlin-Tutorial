12fedda74ab88d3056a455b77644896949d9bea0
jeet404 <jeet63240@gmail.com>
2022-11-20 18:48:06 +0530

    Add Code for Arrays.

---
+++ 17_Arrays.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var arr = arrayOf<String>("Hello", "World", "Kotlin")
+    var arrInt = arrayOf<Int>(1, 6, 7)
+
+    // Set and Get Methods
+    println(arr[2])
+    arr.set(2, "Tony")
+    println(arr[2])
+    println(arrInt.get(2))
+}
